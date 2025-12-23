bd2952129f6c7bee873d0f4a6667aa790b5c1892
rendznicoy <rendzdelosreyes@gmail.com>
2025-12-23 17:11:12 +0800

    [UPDATE] [VERSION 0.0.1] - Added my company assets and removed template assets. Modified assets index.js to use new assets and remove old ones. Modified constats index.js to update some details, other details will be updated tomorrow.

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,8 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import goodpage from "./company/goodpage.png";
+import wela from "./company/wela.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +50,8 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  goodpage,
+  wela,
   carrent,
   jobit,
   tripguide,
